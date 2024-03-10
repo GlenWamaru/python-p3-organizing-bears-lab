e2affca75ea45b7bfa58af8f14cf65956559dd35
SengokuGarp <garpirungu@gmail.com>
2024-03-10 17:03:02 +0300

    Initial commit

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "Sex", "Brown", "Cheerful", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 3, "M", "White", "energetic", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 7, "F", "Black", "gentle", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 6, "M", "brown", "brave", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 9, "F", "golden", "sweet", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 10, "M", "Black", "grumpy", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 6, "F", "White", "cheerful", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("unnamed", NULL, "F", "unknown", "mysterious", 1);
